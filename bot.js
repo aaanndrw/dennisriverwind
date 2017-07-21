73a8b4b970f6a6301b3444b7d2edd9ce47843931
aaanndrw <30334230+aaanndrw@users.noreply.github.com>
2017-07-21 15:49:09 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ var botID = process.env.BOT_ID;
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\@Dennis Reynolds$/ || botRegex = /^\$Dennis$/
+      botRegex = /^\@Dennis Reynolds$/ || botRegex = /$Dennis$/
 
   if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
